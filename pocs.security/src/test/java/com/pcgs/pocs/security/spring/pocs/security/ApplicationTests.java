2ed1ce9f0dcb2d8d883cf5278818afba47954b88
San Gan <32886735+PSANGAN@users.noreply.github.com>
2025-08-06 19:48:00 -0400

    Initial

---
+++ pocs.security/src/test/java/com/pcgs/pocs/security/spring/pocs/security/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcgs.pocs.security.spring.pocs.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
